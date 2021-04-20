ff9c50678124d22d80d60b516cac9a2a7e98cd95
alvar <alvaro.monserrat@gmail.com>
2021-04-20 13:50:21 -0400

    Dialogo para seleccionar camara o gallery 20%

--- app/src/main/java/com/rrat/mvvm2demo/view/activites/AddUpdateDishActivity.kt
+++ app/src/main/java/com/rrat/mvvm2demo/view/activites/AddUpdateDishActivity.kt
@@ -2,11 +2,13 @@ package com.rrat.mvvm2demo.view.activites
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.rrat.mvvm2demo.R
 import com.rrat.mvvm2demo.databinding.ActivityAddUpdateDishBinding
 
 
-class AddUpdateDishActivity : AppCompatActivity() {
+class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityAddUpdateDishBinding
 
@@ -16,6 +18,30 @@ class AddUpdateDishActivity : AppCompatActivity() {
         binding = ActivityAddUpdateDishBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
+
+        setupActionBar()
+
+        binding.ivAddDishImage.setOnClickListener(this)
+    }
+
+    private fun setupActionBar(){
+        setSupportActionBar(binding.toolbarAddDishActivity)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        binding.toolbarAddDishActivity.setNavigationOnClickListener {
+            onBackPressed()
+        }
+    }
+
+    override fun onClick(v: View?) {
+        if(v != null){
+            when(v.id){
+                R.id.iv_add_dish_image ->{
+                    Toast.makeText(this,    "You have clicked the image", Toast.LENGTH_SHORT).show()
+                    return
+                }
+            }
+        }
     }
 
 
