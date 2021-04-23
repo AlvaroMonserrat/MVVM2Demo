43c577287b47ef88cd5f4e82c12338062a16f6a3
alvar <alvaro.monserrat@gmail.com>
2021-04-22 23:12:52 -0400

    Permisos camara y galleria usando Dexter

--- app/src/main/java/com/rrat/mvvm2demo/view/activites/AddUpdateDishActivity.kt
+++ app/src/main/java/com/rrat/mvvm2demo/view/activites/AddUpdateDishActivity.kt
@@ -1,13 +1,24 @@
 package com.rrat.mvvm2demo.view.activites
 
+import android.app.AlertDialog
 import android.app.Dialog
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
 import android.view.View
 import android.widget.Toast
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 import com.rrat.mvvm2demo.R
 import com.rrat.mvvm2demo.databinding.ActivityAddUpdateDishBinding
 import com.rrat.mvvm2demo.databinding.DialogCustomImageSelectionBinding
+import java.lang.Exception
 
 
 class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
@@ -54,17 +65,81 @@ class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
         dialog.setContentView(binding.root)
 
         binding.tvCamera.setOnClickListener {
-            Toast.makeText(this, "Camera clicked", Toast.LENGTH_SHORT).show()
+            Dexter.withContext(this)
+                .withPermissions(
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE,
+                    android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    android.Manifest.permission.CAMERA
+                ).withListener(object : MultiplePermissionsListener{
+                    override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                        if(report!!.areAllPermissionsGranted()){
+                            Toast.makeText(this@AddUpdateDishActivity,
+                                "You have camera permission now",
+                                Toast.LENGTH_SHORT).show()
+                        }
+                    }
+
+                    override fun onPermissionRationaleShouldBeShown(
+                        permissions: MutableList<PermissionRequest>?,
+                        token: PermissionToken?
+                    ) {
+                        // Show Alert Dialog
+                        showRationalDialogPermissions()
+                    }
+
+                }).onSameThread().check()
+
             dialog.dismiss()
         }
 
         binding.tvGallery.setOnClickListener {
-            Toast.makeText(this, "Gallery clicked", Toast.LENGTH_SHORT).show()
+            Dexter.withContext(this)
+                .withPermissions(
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE,
+                    android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ).withListener(object : MultiplePermissionsListener{
+                    override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                        if(report!!.areAllPermissionsGranted()){
+                            Toast.makeText(this@AddUpdateDishActivity,
+                                "You have the Gallery permission now",
+                                Toast.LENGTH_SHORT).show()
+                        }
+                    }
+
+                    override fun onPermissionRationaleShouldBeShown(
+                        permissions: MutableList<PermissionRequest>?,
+                        token: PermissionToken?
+                    ) {
+                        // Show Alert Dialog
+                        showRationalDialogPermissions()
+                    }
+
+                }).onSameThread().check()
+
             dialog.dismiss()
         }
 
         dialog.show()
 
+    }
+
+    private fun showRationalDialogPermissions(){
+        AlertDialog.Builder(this).setMessage("It Looks like you have turned off permissions" +
+                "required for this feature. It can be enabled under Application Settings")
+            .setPositiveButton("GO TO SETTINGS"){
+                _,_ ->
+                try {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package", packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
+                }catch (e:ActivityNotFoundException){
+                    e.printStackTrace()
+                }
+            }
+            .setNegativeButton("Cancel"){
+                    dialog,_-> dialog.dismiss()
+            }.show()
     }
 
 
