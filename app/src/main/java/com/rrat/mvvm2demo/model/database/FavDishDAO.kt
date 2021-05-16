ce9a12d279c7371d383bb01487323f5800ea4a32
alvaro monserrat <alvaro.monserrat@gmail.com>
2021-05-15 23:54:58 -0400

    Se implementa ViewModel

--- app/src/main/java/com/rrat/mvvm2demo/model/database/FavDishDAO.kt
+++ app/src/main/java/com/rrat/mvvm2demo/model/database/FavDishDAO.kt
@@ -2,6 +2,7 @@ package com.rrat.mvvm2demo.model.database
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 import com.rrat.mvvm2demo.model.entities.FavDish
 
 @Dao
@@ -10,5 +11,7 @@ interface FavDishDAO {
     @Insert
     suspend fun insertFavDishDetails(favDish: FavDish)
 
+    @Query("SELECT * FROM  FAV_DISHES_TABLE ORDER BY ID")
+    fun getAllDishesList() : kotlinx.coroutines.flow.Flow<List<FavDish>>
 
 }
--- app/src/main/java/com/rrat/mvvm2demo/model/database/FavDishRepository.kt
+++ app/src/main/java/com/rrat/mvvm2demo/model/database/FavDishRepository.kt
@@ -2,6 +2,7 @@ package com.rrat.mvvm2demo.model.database
 
 import androidx.annotation.WorkerThread
 import com.rrat.mvvm2demo.model.entities.FavDish
+import kotlinx.coroutines.flow.Flow
 
 class FavDishRepository(private val favDishDAO: FavDishDAO) {
 
@@ -9,5 +10,7 @@ class FavDishRepository(private val favDishDAO: FavDishDAO) {
     suspend fun insertFavDishData(favDish: FavDish){
         favDishDAO.insertFavDishDetails(favDish)
     }
+
+    val allDishesList: Flow<List<FavDish>> = favDishDAO.getAllDishesList()
 
 }
